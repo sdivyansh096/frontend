6fcc8e92944138826fa5a015c2fd4e80891e1408
sdivyanshhehe <sdivyansh096@gmail.com>
2024-11-11 12:19:12 +0530

    changes

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,19 +1,19 @@
-import Chart from "chart.js/auto";
+// import Chart from "chart.js/auto";
 import { CategoryScale } from "chart.js";
 import { useState } from "react";
 import { Data } from "./Data";
 import LineChart from "./LineChart";
-import "./styles.css";
+// import "./styles.css";
 
 // Chart.register(CategoryScale);
 
-function Chart() {
+const Chart = () => {
   const [chartData, setChartData] = useState({ Data });
 
   return (
-    <div className="App">
+    <div>
       <LineChart chartData={chartData} />
     </div>
   );
-}
+};
 export default Chart;
